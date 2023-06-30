570dd20eca2f78e97884ab6dedb8b84ed32150ce
AndrzejHanaj <andrzej.hanaj@op.pl>
2023-06-30 21:58:24 +0200

    commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ class Slider {
         this.prevBtn = null;
         this.nextBtn = null;
         this.image = null;
+        this.currentSlide = 0;
 
         this.UiSelectors = {
             slide: '[data-slide]',
@@ -21,9 +22,25 @@ class Slider {
         const figure = document.getElementsByTagName('figure');
 
         this.image = document.createElement('img');
-        this.image.classList.add('slide_image')
-        this.image.setAttribute('src', this.images[0]);
+        this.image.classList.add('slide_image');
+        this.setSlideAtributes(0);
 
        this.slide.appendChild(this.image);
+       this.addListeners;
+    }
+    changeSlide(index){
+    this.currentSlide = index;
+    this.setSlideAtributes(index);
+
+    }
+
+    addListeners(){
+        this.prevBtn.addEventListener('click', () => this.changeSlide(this.currentSlide - 1));
+        this.nextBtn.addEventListener('click', () => this.changeSlide(this.currentSlide + 1));
+    }
+
+    setSlideAtributes(index){
+    this.image.setAttribute('src', Array.isArray(this.images) && this.images.length && this.images[index]);
+    this.image.setAttribute('alt', `Slide ${index + 1}`);
     }
 }
